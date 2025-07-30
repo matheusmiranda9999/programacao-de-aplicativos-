665237e8c4770e2218bc85266e7d907e8a2f4e4f
matheusmiranda9999 <mirandamatheus482@gmail.com>
2025-07-30 11:17:19 -0300

    Create Lista1E.js

---
+++ Lista1E.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync") ();
+let distância = Number(prompt("digite a distância da viagem (em km):"));
+let consumo = Number(prompt("Digite o consumo
+médio do
+carro (km por litro):"));
+let preço = Number (prompt ("Digite o preço da
+gasolina(R$ por litro):"));
+let litros_necessarios - distancia / consumo; let custo_total - litros_necessarios * preco; console. log("Litros necessários:", litros necessarios,"L";
+console. log("Custo total da viagem: R$",custo total);
